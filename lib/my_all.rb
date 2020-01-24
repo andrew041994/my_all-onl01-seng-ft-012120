3928f657bd3d67ac26169264d7d52c46d99fd2f4
Alehandro Persaud <andrew041994@gmail.com>
2020-01-24 03:17:53 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -2,11 +2,15 @@ require 'pry'
 
 def my_all?(collection)
   i=0
+  result = []
   while i < collection.size
-    yield(collection[i])
+    result << yield(collection[i])
     i+=1
   end
 
-
-
+  if result.include?(false)
+    false
+  else
+    true
+  end
 end
